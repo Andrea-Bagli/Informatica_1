5ce58e54c0729e8c04614a1e6212180b148548bf
Andrea-Bagli <113690918+Andrea-Bagli@users.noreply.github.com>
2022-10-19 11:46:08 +0200

    Add files via upload

---
+++ stampa_numeri_con_spazi.cpp
@@ -0,0 +1,20 @@
+/******************************************
+******************************************/
+#include <stdio.h>
+int main()
+{
+
+int num ;//input
+int num1, num2, num3;
+
+printf("inserisci un numero compreso tra 100 e 999 \n ");
+scanf("%d", & num);
+
+num1= num / 100;
+num2=(num-num1 * 100) /10;
+num3= (num-(num1 * 100 + num2 *10));
+printf("%d \t %d \t %d \t", num1, num2, num3);
+return 0;
+}
+
+
